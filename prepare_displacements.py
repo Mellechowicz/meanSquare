eef13b8b613e39188e608239cc29833c3b297ee1
Andrzej P. Kądzielawa <apkadzielawa@gmail.com>
2022-06-15 13:32:28 +0200

    Added new func: interpolating mean squared disp form qha data

--- prepare_displacements.py
+++ prepare_displacements.py
@@ -16,7 +16,12 @@ def symmetric_matix(vector):
 
 cmdLine = argv.Options()
 
-for yaml_file in cmdLine('files'):
+if cmdLine('qha'):
+    allDisplacements = []
+    fileVolumes = np.loadtxt(cmdLine('e_v'))[:,0]
+    volumeTemp  = np.loadtxt(cmdLine('volume_temperature'))
+
+for idx,yaml_file in enumerate(cmdLine('files')):
     with open(yaml_file) as file:
         atoms = cmdLine('indexes')
         try:
@@ -40,24 +45,62 @@ for yaml_file in cmdLine('files'):
 
             np.savetxt(yaml_file[:-5]+'.txt',displacements)
 
-            if not cmdLine('plot'):
-                continue
-    
-            import matplotlib.pyplot as plt
-            fig,ax = plt.subplots()
-            labels = cmdLine('labels')
-            diff   = len(atoms)-len(labels) 
-            if diff > 0:
-                for i in range(len(atoms)-diff,len(atoms)):
-                    labels.append("atom #%d"%atoms[i])
-            labels = labels[:len(atoms)]
-            for i,label in enumerate(labels):
-                ax.plot(displacements[:,0],displacements[:,i+1],label=label)
-                ax.set_title("Mean-square displacement")
+            if cmdLine('plot'):
+                import matplotlib.pyplot as plt
+                fig,ax = plt.subplots()
+                labels = cmdLine('labels')
+                diff   = len(atoms)-len(labels) 
+                if diff > 0:
+                    for i in range(len(atoms)-diff,len(atoms)):
+                        labels.append("atom #%d"%atoms[i])
+                labels = labels[:len(atoms)]
+                for i,label in enumerate(labels):
+                    ax.plot(displacements[:,0],displacements[:,i+1],label=label)
+                if not cmdLine('qha'):
+                    ax.set_title("Mean-square displacement (constant volume)")
+                else:
+                    ax.set_title("Mean-square displacement (V=%9.0f A^3)"%fileVolumes[idx])
                 ax.set_xlabel("temperature, T (K)")
                 ax.set_ylabel("displacement, d^2 (A^2)")
-            ax.grid()
-            ax.legend()
-            plt.savefig(yaml_file[:-5]+'.png',dpi=300)
+                ax.grid()
+                ax.legend()
+                plt.savefig(yaml_file[:-5]+'.png',dpi=300)
+
+            if cmdLine('qha'):
+                allDisplacements.append(displacements)
         except yaml.YAMLError as exc:
             print(exc)
+
+if cmdLine('qha'):
+    allDisplacements = np.array(allDisplacements)
+    if fileVolumes.shape[0] != allDisplacements.shape[0]:
+        raise IndexError("The data in "+cmdLine('e_v')+" does not match provided files!")
+    from scipy.interpolate import interp1d
+    temperatures     = allDisplacements[0,:,0]
+    qhaDisplacements = []
+    for i,T in enumerate(temperatures):
+        volume        = volumeTemp[volumeTemp[:, 0] == T][0,1]
+        displacements = allDisplacements[:,i,1:]
+        interpolation = np.array([ interp1d(fileVolumes,displacements[:,j])(volume) for j,_ in enumerate(displacements[0,:]) ])
+        qhaDisplacements.append([T,volume,*interpolation])
+    qhaDisplacements = np.array(qhaDisplacements)
+    if cmdLine('plot'):
+        import matplotlib.pyplot as plt
+        fig,ax = plt.subplots()
+        for i,label in enumerate(labels):
+            ax.plot(qhaDisplacements[:,0],qhaDisplacements[:,i+2],label=label)
+        ax.set_title("Mean-square displacement (Quasiharmonic approximation)")
+        ax.set_xlabel("temperature, T (K)")
+        ax.set_ylabel("displacement, d^2 (A^2)")
+
+        ax2 = ax.twinx()
+        ax2.plot(qhaDisplacements[:,0],qhaDisplacements[:,1],'--',c='k',label='volume')
+        ax2.set_ylabel("volume, V (A^3)")
+        ax.plot([],[],'--',c='k',label='cell volume') # Just for the legend
+        limits = ax2.get_ylim()
+        limits = (1.3*limits[0]-0.3*limits[1],limits[1]) # so that volume doesn't overlap with the displacements
+        ax2.set_ylim(limits)
+
+        ax.grid()
+        ax.legend()
+        plt.savefig('qha_displacement.png',dpi=300)
